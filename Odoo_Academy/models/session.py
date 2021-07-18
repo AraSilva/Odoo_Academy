a9f1f99ed774889cdfa94ab5300d219d8c9a769b
Araceli Silvas <araceli.silva@emporioalemanpy.com>
2021-07-18 14:17:05 -0400

    pivot view

--- Odoo_Academy/models/session.py
+++ Odoo_Academy/models/session.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from datetime import timedelta
 
 class Session(models.Model):
     _name = 'academy.session'
@@ -16,3 +16,32 @@ class Session(models.Model):
     instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
 
     student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
+
+    start_date = fields.Date(string='Start Date',
+                             default=fields.Date.today)
+
+    duration = fields.Integer(string='Session Days',
+                              default=1)
+
+    end_date = fields.Integer(string='End Date',
+                              compute='_compute_end_date',
+                              inverse='_inverse_end_date',
+                              store=True)
+
+    @api.depends('start_date', 'duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+            else:
+                duration = timedelta(days=record.duration)
+                record.end_date = record.start_date + duration
+
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days + 1
+            else:
+                continue
+
+
