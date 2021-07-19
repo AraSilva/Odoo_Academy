76740e00ffa5bf925b23daa5e7ae230963819242
Araceli Silvas <araceli.silva@emporioalemanpy.com>
2021-07-19 11:31:08 -0400

    sale order inherit

--- Odoo_Academy/__manifest__.py
+++ Odoo_Academy/__manifest__.py
@@ -1,17 +1,18 @@
 {
     'name': 'Odoo Academy',
-    'summary': 'Academy app to manage Training',
+    'summary': """Academy app to manage Training""",
     'description': "Academy Module to manage Training",
     'author': 'Odoo',
     'category': 'Training',
     'version': '1.0',
-    'depends': ['base'],
+    'depends': ['sale'],
     'data': [
         'views/course_views.xml',
         'views/academy_menuitems.xml',
         'security/academy_security.xml',
         'security/ir.model.access.csv',
-        'views/session_views.xml'
+        'views/session_views.xml',
+        'views/sale_views_inherit.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
--- Odoo_Academy/models/__init__.py
+++ Odoo_Academy/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import course
 from . import session
+from . import sale_order
