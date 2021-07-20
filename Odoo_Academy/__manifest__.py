a580310ac7f564bfeafd78344e4bb074c56d0b6f
Araceli Silvas <araceli.silva@emporioalemanpy.com>
2021-07-20 15:22:13 -0400

    session report dia 8

--- Odoo_Academy/__manifest__.py
+++ Odoo_Academy/__manifest__.py
@@ -15,6 +15,7 @@
         'views/sale_views_inherit.xml',
         'views/product_views_inherit.xml',
         'wizard/sale_wizard_view.xml',
+        'report/session_report_templates.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
